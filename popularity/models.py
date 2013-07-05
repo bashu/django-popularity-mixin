12aff9ccb86102bafc4686908ffe28621af2c8e2
Basil Shubin <basil.shubin@gmail.com>
2013-07-05 21:07:50 +0700

    fix typo

--- popularity/models.py
+++ popularity/models.py
@@ -5,11 +5,11 @@ from django.db.models.signals import post_save
 
 from hitcount.models import Hit
 
+from .tasks import HitCountJob
+
 
 @receiver(post_save, sender=Hit)
 def invalidate(sender, instance, **kwargs):
     if instance is not None and isinstance(instance, Hit):
-        from .tasks import HitCount
-
         opts, object_id = instance._meta, instance.pk,
-        HitCount().invalidate(opts.app_label, opts.module_name, object_id)
+        HitCountJob().invalidate(opts.app_label, opts.module_name, object_id)
