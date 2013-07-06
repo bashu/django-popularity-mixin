73898461d6238d7a91fc1b2c1e5e060125a264b8
Basil Shubin <basil.shubin@gmail.com>
2013-07-06 12:26:12 +0700

    testing web hook

--- popularity/tests/test_views.py
+++ popularity/tests/test_views.py
@@ -3,3 +3,12 @@
 from django.test import TestCase
 
 from ..views import PopularityMixin
+
+
+class PopularityMixinTest(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_default(self):
+        pass
