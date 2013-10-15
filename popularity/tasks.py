3e1e2ff85c5ed55eadb1f34bd3d29696cbe20cce
oldcai <albert.cai@auramarker.com>
2013-10-15 17:51:36 +0800

    Configurable hitcount cache lifetime that respect "HITCOUNT_REFRESH_INTERVAL" in settings

--- popularity/tasks.py
+++ popularity/tasks.py
@@ -71,6 +71,10 @@ def update_hitcount(session_key, ip_address, user_agent, username,
 
 class HitCountJob(Job):
 
+    def __init__(self, *args, **kwargs):
+        self.lifetime = getattr(settings, 'HITCOUNT_REFRESH_INTERVAL', 600)
+        return super(HitCountJob, self).__init__(*args, **kwargs)
+
     def fetch(self, app_label, model, object_id):
         ctype = ContentType.objects.get(app_label=app_label, model=model)
         try:
