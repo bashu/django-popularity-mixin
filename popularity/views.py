f0a2888e9ed78933c77e8cf9d9b0f8d05c226577
Basil Shubin <basil.shubin@gmail.com>
2014-03-22 17:57:56 +0700

    fix import

--- popularity/views.py
+++ popularity/views.py
@@ -8,15 +8,15 @@ class PopularityMixin(object):
     def get(self, request, *args, **kwargs):
         response = super(PopularityMixin, self).get(request, *args, **kwargs)
         if getattr(settings, 'USE_HITCOUNT') and hasattr(self, 'object'):
-            from .tasks import update_hitcount
             from hitcount.utils import get_ip
+            from popularity.tasks import celery_update_hitcount
 
             opts, u = self.object._meta, request.user
 
             if not request.session.session_key:
                 request.session.save()
 
-            update_hitcount.delay(
+            celery_update_hitcount.delay(
                 session_key=request.session.session_key,
                 ip_address=get_ip(request),
                 user_agent=request.META.get('HTTP_USER_AGENT', '')[:255],
@@ -31,7 +31,7 @@ class PopularityMixin(object):
     @classmethod
     def get_hitcount_for(cls, obj):
         if getattr(settings, 'USE_HITCOUNT', False):
-            from .tasks import HitCountJob
+            from popularity.tasks import HitCountJob
 
             opts, pk = obj._meta, obj.pk
             return HitCountJob().get(opts.app_label, opts.module_name, pk)
