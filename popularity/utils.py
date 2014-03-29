e36ccd34c129786ca223009f78f42e4434a67df9
Basil Shubin <basil.shubin@gmail.com>
2014-03-29 13:14:41 +0700

    fix imports

--- popularity/utils.py
+++ popularity/utils.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import AnonymousUser, User
 
