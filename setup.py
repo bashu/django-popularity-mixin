ed6bf164ca5771e43654201db9d178414c432658
Basil Shubin <basil.shubin@gmail.com>
2014-03-29 12:57:04 +0700

    bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-popularity-mixin',
-    version='0.1.4',
+    version='0.1.4.1',
     packages=['popularity'],
     include_package_data=True,
     license='GNU Library or LGPL License',
